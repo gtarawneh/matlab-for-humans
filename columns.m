c6d70401aed472c93d322b8316f412f8917670a5
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-02-05 23:14:38 +0000

    Add rows and columns

---
+++ columns.m
@@ -0,0 +1,7 @@
+% Return number of columns in matrix.
+
+function n = columns(m)
+
+	n = size(m, 2);
+
+end
---
+++ rows.m
@@ -0,0 +1,7 @@
+% Return number of rows in matrix.
+
+function n = rows(m)
+
+	n = size(m, 1);
+
+end
