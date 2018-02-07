fec3bc5360cab8e216c44b7b16c627ba8937a590
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-02-07 22:50:36 +0000

    Fix islist and len

--- islist.m
+++ islist.m
@@ -1,9 +1,11 @@
+% Return 1 if object is a list, 0 otherwise.
+
 function result = islist(object)
 
-non_singular_dims = size(object) > 1;
+% non_singular_dims = size(object) >= 1;
 
-is_matrix = isequal(non_singular_dims, [1 1]);
+% result = isequal(non_singular_dims, [1 1]);
 
-result = is_matrix || isvector(object);
+result = isequal(size(size(object)), [1 2]);
 
 end
--- len.m
+++ len.m
@@ -2,6 +2,6 @@ function result = len(list)
 
 	assert(islist(list), 'input must be a list');
 
-	result = ifelse(isvector(list), length(list), size(list, 1));
+	result = size(list, 1);
 
 end
